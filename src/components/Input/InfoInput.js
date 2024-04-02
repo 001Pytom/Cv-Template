8ce7fe13abae7b551690dd56924c6065be85dad5
faatihat <tomfaah@gmail.com>
2024-04-02 13:44:22 +0100

    code adjustment

--- src/components/Input/InfoInput.js
+++ src/components/Input/InfoInput.js
@@ -1,30 +1,36 @@
-import React from "react";
-
-function InfoInput({ onAddInput }) {
-  const handleChange = (e) => {
-    onAddInput(e.target.name, e.target.value);
-  };
-
+function InfoInput({ info, onAddInput }) {
   return (
     <div className="general-info personal-input">
       <div>
-        Personal Information <button>💻</button>
+        Personal information <button>💻</button>
       </div>
-      {[
-        { name: "Name", type: "text", placeholder: "Name" },
-        { name: "Email", type: "email", placeholder: "Email" },
-        { name: "Phone", type: "number", placeholder: "Phone Number" },
-      ].map((field, index) => (
-        <div key={index}>
-          <span>{field.name}:</span>
-          <input
-            name={field.name.toLowerCase()}
-            type={field.type}
-            placeholder={field.placeholder}
-            onChange={handleChange}
-          />
-        </div>
-      ))}
+      <div>
+        <span>Name:</span>
+        <input
+          name="name"
+          type="text"
+          placeholder="Name"
+          onChange={onAddInput}
+        />
+      </div>
+      <div>
+        <span>Email:</span>
+        <input
+          name="email"
+          type="email"
+          placeholder="Email"
+          onChange={onAddInput}
+        />
+      </div>
+      <div>
+        <span>Phone:</span>
+        <input
+          name="phone"
+          type="number"
+          placeholder="Phone Number"
+          onChange={onAddInput}
+        />
+      </div>
     </div>
   );
 }
